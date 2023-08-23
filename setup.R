120912e80f7abd44fdb4c83b86fb5c6edf9f3d84
elizabethng <eng@fourpeaksenv.com>
2023-08-23 07:36:19 -0700

    Add renv setup info

--- setup.R
+++ setup.R
@@ -1,5 +1,8 @@
 # Install needed packages from forked repositories
 
+# For new instances of the project, use renv::restore()
+# see https://rstudio.github.io/renv/articles/renv.html
+
 # Use the Four Peaks versions
 renv::install("car")
 renv::install("curl")
